5dbb6d96b7b90f19d10142a6574ae2c886bffbf4
Divis Tencent Server <6171586-web@users.noreply.github.com>
2026-09-16 13:18:11 +0800

    Publish daily from Tencent server

--- site/data.js
+++ site/data.js
@@ -1,6 +1,6 @@
 window.DIVIS_AI_DATA = {
   "updatedAt": "2026-09-15",
-  "lastCheckedAt": "2026-09-16 12:18",
+  "lastCheckedAt": "2026-09-16 13:18",
   "fundBaseNav": 1.0009620009620008,
   "priceBase": 5120.71,
   "points": [
